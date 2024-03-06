04c6416fb20dde22ec6dcd99a3a73af2d4df61c5
Bayu Kurniawan <bayu.kurniawan@taksu.tech>
2024-03-06 13:57:06 +0800

    remove build

--- build/app/controllers/todos_controller.js
+++
@@ -1,7 +0,0 @@
-import Todo from '#models/todo';
-import BaseController from './base_controller.js';
-export default class TodosController extends BaseController {
-    model = Todo;
-    relationships = ['user', 'category'];
-}
-//# sourceMappingURL=todos_controller.js.map
--- build/app/controllers/users_controller.js
+++
@@ -1,6 +0,0 @@
-import User from '#models/user';
-import BaseController from './base_controller.js';
-export default class UsersController extends BaseController {
-    model = User;
-}
-//# sourceMappingURL=users_controller.js.map
